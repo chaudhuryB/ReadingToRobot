064bf7eb216b9f4e7e9f034cb7bd376995fc2c1a
Bishakha <35263454+chaudhuryB@users.noreply.github.com>
2020-07-23 16:07:59 +0100

    Nao's Choregraphe behaviour

---
+++ Nao_Behaviours/Dab/translations/translation_en_US.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Elephant/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>That was awesome!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That was awesome!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That was awesome!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That was awesome!</translation>
+        </message>
+    </context>
+</TS>
---
+++ Nao_Behaviours/Thankyou/translations/translation_en_US.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Great job! Thankyou</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Great job! Thankyou</translation>
+        </message>
+        <message>
+            <source>Great job! Thankyou!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Great job! Thankyou!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Great job! Thank you!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Great job! Thank you!</translation>
+        </message>
+    </context>
+</TS>
