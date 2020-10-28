28aa0cbf6e39ac024651374a285d71a727bf25eb
Aitor Miguel Blanco <aitormibl@gmail.com>
2020-10-28 11:27:08 +1100

    Moved keyboard_control to common
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

--- Cozmo/game_engine.py
+++ Cozmo/game_engine.py
@@ -2,26 +2,18 @@ import asyncio
 import copy
 import cozmo
 import time
-import threading
+from threading import Lock
 from cozmo.util import degrees, distance_mm
-from random import randint
-from keyboard_control import EmotionController
-
+from ..common.keyboard_control import EmotionController, feel
 
 from constants import (
-                        START_CUBE,
-                        END_CUBE,
-                        RED_LIGHT,
-                        GREEN_LIGHT,
-                        SEA_LIGHT,
-                        CHOICE_TEXT,
-                        NEUTRAL,
-                        HAPPY,
-                        SAD,
-                        ANNOYED,
-                        SCARED,
-                        EXCITED,
-                        )
+                       START_CUBE,
+                       END_CUBE,
+                       RED_LIGHT,
+                       GREEN_LIGHT,
+                       SEA_LIGHT,
+                       CHOICE_TEXT,
+                       )
 from cozmo_listener import CozmoPlayerActions
 from voiceRecognition import SpeechReco
 
@@ -33,18 +25,19 @@ class ReadEngine:
         self.robot_cubes = []
         self.my_postiion = None
         self.face = None
-        self.feel = NEUTRAL
+        self.feel = feel.NEUTRAL
+        self.lock = Lock()
 
     def tap_ready(self):
         player_tapped = False
-        light_cube_list = [cozmo.objects.LightCube1Id, cozmo.objects.LightCube2Id ,cozmo.objects.LightCube3Id ]
+        light_cube_list = [cozmo.objects.LightCube1Id, cozmo.objects.LightCube2Id, cozmo.objects.LightCube3Id]
         for lightcube_id in light_cube_list:
             cube = self.robot.world.light_cubes.get(lightcube_id)
             self.robot_cubes.append(cube)
             cube.start_light_chaser(START_CUBE)
 
         try:
-            tapped_event = self.robot.world.wait_for(cozmo.objects.EvtObjectTapped,timeout=120)
+            tapped_event = self.robot.world.wait_for(cozmo.objects.EvtObjectTapped, timeout=120)
             if tapped_event:
                 player_tapped = True
 
@@ -72,7 +65,7 @@ class ReadEngine:
         self.robot_proxy = CozmoPlayerActions()
 
         self.read_listener = EmotionController(self.robot_proxy,
-                                        self)
+                                               self)
         if self.robot.is_on_charger:
             self.robot.DriveOffChargerContacts().wait_for_completed()
             robot.drive_straight(distance_mm(800), speed_mmps(500)).wait_for_completed()
@@ -98,7 +91,7 @@ class ReadEngine:
             """
 
             face = self.robot.world.wait_for_observed_face(timeout=600, include_existing=True)
-            self.face=face
+            self.face = face
             self.robot_proxy.set_robot(self.robot, self.face)
             self.robot.turn_towards_face(face).wait_for_completed()
             self.robot.play_anim("anim_greeting_happy_03").wait_for_completed()
@@ -108,44 +101,47 @@ class ReadEngine:
             print("Didn't find any face :-(")
 
         finally:
-            #look_around.stop()
-            if self.face == None:
+            # look_around.stop()
+            if self.face is None:
                 print("Didn't find anyone :-(")
                 return False
 
         return True
 
+    def do_feel(self, feel):
+        with self.lock:
+            self.feel = feel
+
     def listen_to_story(self):
         try:
-            self.read_listener.game_on =True
+            self.read_listener.game_on = True
             self.read_listener.start()
             while True:
-                if self.feel == NEUTRAL:
+                with self.lock:
+                    f = self.feel
+                if f == feel.NEUTRAL:
                     self.robot_proxy.do_listen()
                     time.sleep(1.0)
-                elif self.feel == HAPPY:
+                elif f == feel.HAPPY:
                     self.robot_proxy.be_happy()
-                    self.feel = NEUTRAL
-                elif self.feel == SAD:
+                elif f == feel.SAD:
                     self.robot_proxy.be_sad()
-                    self.feel = NEUTRAL
-                elif self.feel == ANNOYED:
+                elif f == feel.ANNOYED:
                     self.robot_proxy.be_annoyed()
-                    self.feel = NEUTRAL
-                elif self.feel == SCARED:
+                elif f == feel.SCARED:
                     self.robot_proxy.be_scared()
-                    self.feel = NEUTRAL
-                elif self.feel == EXCITED:
+                elif f == feel.EXCITED:
                     self.robot_proxy.be_excited()
-                    self.feel = NEUTRAL
                 """
-                elif self.feel == SLEEPY:
+                elif f == feel.SLEEPY:
                     self.robot_proxy.go_to_sleep()
                     break;
                 """
+                with self.lock:
+                    f = feel.NEUTRAL
 
         except Exception as e:
-            self.feel = NEUTRAL
+            self.feel = feel.NEUTRAL
             print(e)
 
         except KeyboardInterrupt:
@@ -160,4 +156,3 @@ class ReadEngine:
 
             self.end_session()
             self.robot_proxy.do_fist_bump()
-
