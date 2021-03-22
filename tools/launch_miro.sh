8756c947f0038386fd2a226a03dcada441579c71
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-03-22 16:42:21 +1100

    Write logger output to file
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

--- tools/launch_miro.sh
+++ tools/launch_miro.sh
@@ -4,4 +4,5 @@ LOCAL_IP=$(hostname -I | awk '{print $1;}')
 
 docker run --env MIRO_ROBOT_IP=$1 --env MIRO_LOCAL_IP=$LOCAL_IP \
            --device /dev/snd:/dev/snd --network="host" \
+           -v /home/ubuntu/logs:/logs \
            -it reading bash -c "source /root/mdk/setup.bash && read_to_miro"
