ed288495adeeefa12aacced844efdcb3967e6915
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-03-29 17:49:00 +1100

    Load ros and mdk in launch
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

--- tools/launch_miro.sh
+++ tools/launch_miro.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+source /opt/ros/noetic/setup.bash
+source /home/ubuntu/mdk/setup.bash
+
 export ROS_IP=$(hostname -I | awk '{print $1;}')
 export ROS_MASTER_URI=http://$1:11311
+
 read_to_miro
